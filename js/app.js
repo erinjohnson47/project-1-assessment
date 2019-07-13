8e832691e885ccafb46de6dd62656a86f60bae6d
Erin Johnson <erinjohnson47@gmail.com>
2019-07-13 14:42:20 -0700

    js logic complete, buttons function, numbers add/sub, display

--- js/app.js
+++ js/app.js
@@ -1,1 +1,26 @@
 console.log('This JS file has loaded.')
+
+const num = document.querySelector('.num'); //variable for input box
+const add = document.querySelector('.add'); //variable for + button
+const sub = document.querySelector('.sub'); //variable for - button
+let result = document.querySelector('#result'); //variable for results
+let finalResult = 0;
+
+result.innerText = 0;
+num.value = 1;
+
+document.addEventListener('click', e => {
+    switch (e.target) {
+        case add:
+            finalResult = parseInt(result.innerText) + parseInt
+            (num.value);
+            console.log(finalResult);
+            result.innerText = finalResult;
+            break;
+        case sub: 
+            finalResult = parseInt(result.innerText) - parseInt(num.value);
+            console.log(finalResult)
+            result.innerText = finalResult;
+            break;
+    }
+})
